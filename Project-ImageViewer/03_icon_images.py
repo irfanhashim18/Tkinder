4ba8ec18133e6f07f6c0a8d78d96296e54269857
Irfan Hashim Sheikho <irfanhashim18@gmail.com>
2021-04-13 12:01:34 -0700

    we worked on images and created a end program buton

--- Project-ImageViewer/03_icon_images.py
+++ Project-ImageViewer/03_icon_images.py
@@ -1,7 +1,32 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 
 root = Tk()
-root.title(" Learning Tkinder ")
+root.title("Image Viewer")
+#this is for fixing the size of the windows
+# if we not use this the window will adjust auto
+root.geometry("500x500")
+root.iconbitmap("imageViewer.ico")
+
+#close program button
+
+button_quit =Button(root, text= "End Program", command = root.quit)
+button_quit.pack()
+
+#for using the picture in python we need to install a full module call pillow
+#pip install pillow
+#for calling it we will use from PIL(python image Library) import things
+
+image_1 = ImageTk.PhotoImage(Image.open("image_1.png"))
+lebal_1 = Label(image=image_1)
+lebal_1.pack()
+
+
+
+
+
+
+
 
 root.mainloop()
